d8318ca86e5c7236f48aff2cc466a02ecd657aad
v.talas <v.talas@gmc.net>
2014-08-28 11:43:42 +0200

    rozsekani na soubory

--- client/src/entryDetail.js
+++ client/src/entryDetail.js
@@ -9,63 +9,6 @@ entryDetail.AlbumDetail = function () {
 
 var gData = {};
 
-(function () {
-
-	gData.Photo = function (data) {
-		this.width = Number(data.gphoto$width.$t, 10);
-		this.height = Number(data.gphoto$height.$t, 10);
-		this.id = data.gphoto$id.$t;
-		this.url = new gData.PhotoUrl(data.media$group.media$content[0].url);
-		this.thumb = data.media$group.media$thumbnail[0].url;
-	};
-	gData.Photo.prototype.toJson = function () {
-		return {u: this.url.get()};
-	};
-
-}());
-
-(function () {
-	gData.PhotoAlbum = function () {
-		this.photos = [];
-		this.title = "";
-	};
-
-	gData.PhotoAlbum.prototype.init = function (data) {
-		var i,
-			photos = data.feed.entry;
-
-		this.title = data.feed.title.$t;
-		for (i = 0; i < photos.length; i++) {
-			this.photos.push(new gData.Photo(photos[i]));
-		}
-	};
-
-	gData.PhotoAlbum.prototype.toJson = function () {
-		var json = [],
-			i;
-
-		for (i = 0; i < this.photos.length; i++) {
-			json.push(this.photos[i].toJson());
-		}
-		return json;
-	};
-
-}());
-
-(function () {
-	gData.PhotoUrl = function (url) {
-		this.url = url;
-		this.urlParts = this.url.split("/");
-
-		this.placeholder = this.urlParts.length - 1;
-		this.urlParts.splice(this.placeholder, 0, "s75");
-	};
-
-	gData.PhotoUrl.prototype.get = function (size) {
-		this.urlParts[this.placeholder] = size || "s75-c";
-		return this.urlParts.join("/");
-	};
-}());
 
 
 entryDetail.getData = function () {
