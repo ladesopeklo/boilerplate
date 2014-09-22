96fb83fc8143853e71cdac991cdb7a0b92ea5ffa
ladeso <vladimir.talas@gmail.com>
2014-09-22 21:49:06 +0200

    oprava photourl

--- client/src/gdata/gdata.photourl.js
+++ client/src/gdata/gdata.photourl.js
@@ -1,29 +1,41 @@
-
+/*global gData*/
 (function () {
-    gData.PhotoUrl = function (url, width, height) {
-        this.url = url;
-        this.urlParts = this.url.split("/");
-        this.width = width;
-        this.height= height;
+	/**
+	 *
+	 * @param url
+	 * @param {number=} width
+	 * @constructor
+	 */
+	gData.PhotoUrl = function (url, width) {
+		this.url = url;
+		this.urlParts = this.url.split("/");
+		this.width = width || null;
 
-        this.placeholderIndex = this.urlParts.length - 1;
-        this.urlParts.splice(this.placeholderIndex, 0, "s75");
-    };
+		this.placeholderIndex = this.urlParts.length - 2;
+		this.default = "s140-c";
+	};
 
-    /**
-     * @param {string|number=} size
-     * -1: fullsize,
-     * empty - default thumb,
-     * w99 - set width to 99
-     * @returns {string} url
-     */
-    gData.PhotoUrl.prototype.get = function (size) {
-        var setSize = size || "s75";
+	gData.PhotoUrl.init = function (url, width) {
+		var withoutDefinedSize = new gData.PhotoUrl(url, width);
+		withoutDefinedSize.placeholderIndex = this.urlParts.length - 1;
+		withoutDefinedSize.urlParts.splice(withoutDefinedSize.placeholderIndex, 0, this.default);
+		return withoutDefinedSize;
+	};
 
-        if (size === -1) {
-            setSize =  "w" + this.width;
-        }
-        this.urlParts[this.placeholderIndex] = setSize;
-        return this.urlParts.join("/");
-    };
+	/**
+	 * @param {string|number=} size
+	 * -1: fullsize,
+	 * empty - default thumb,
+	 * w99 - set width to 99
+	 * @returns {string} url
+	 */
+	gData.PhotoUrl.prototype.get = function (size) {
+		var setSize = size || this.default;
+
+		if (size === -1) {
+			setSize =  "w" + this.width;
+		}
+		this.urlParts[this.placeholderIndex] = setSize;
+		return this.urlParts.join("/");
+	};
 }());
