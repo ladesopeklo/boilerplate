2726993badf07d369c21a04a0f4e9c7af0f2a7cc
ladeso <vladimir.talas@gmail.com>
2014-11-24 23:52:27 +0100

    navbar module

---
+++ client/src/module.navibar.js
@@ -0,0 +1,21 @@
+///<reference path='..\libs\bower_components\mithril\mithril.d.ts'/>
+///<reference path='module.FullSize.ts'/>
+///<reference path='data/data.Repository.ts'/>
+///<reference path='data/data.ImageGallery.ts'/>
+///<reference path='module.Gallery.ts'/>
+///<reference path='windowZepto.ts'/>
+var Navibar;
+(function (Navibar) {
+    var controller = (function () {
+        function controller() {
+        }
+        return controller;
+    })();
+    Navibar.controller = controller;
+    function view(ctrl) {
+        return [
+            m("div", { "class": "xxx" }, "adkjbaksjdbkasbkjd")
+        ];
+    }
+    Navibar.view = view;
+})(Navibar || (Navibar = {}));
---
+++ client/src/module.navibar.ts
@@ -0,0 +1,24 @@
+///<reference path='..\libs\bower_components\mithril\mithril.d.ts'/>
+///<reference path='module.FullSize.ts'/>
+///<reference path='data/data.Repository.ts'/>
+///<reference path='data/data.ImageGallery.ts'/>
+///<reference path='module.Gallery.ts'/>
+///<reference path='windowZepto.ts'/>
+
+module Navibar {
+    export class controller {
+        constructor() {
+
+        }
+    }
+
+    export function view(ctrl:Navibar.controller) {
+        return [
+            m("div", {"class": "xxx"}, "adkjbaksjdbkasbkjd")
+        ];
+    }
+}
+
+
+
+
